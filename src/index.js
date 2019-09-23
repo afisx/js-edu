99901bb9a887bab11dda55bdb246afb9a8523708
afisx <alexej.moiseenko@gmail.com>
2019-09-23 22:00:28 +0300

    feat: implement getTimeForEducation function

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+		const time = (knowsProgramming) ? 800 : 1300;
+		return Math.ceil(time / config[focus]);
   };
   
